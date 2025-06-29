d8431e5819be0f03b9523fd16f9ba024912ae8a2
Poom Yoosorn <poom.yoosorn@gmail.com>
2025-06-29 22:04:06 +0700

    App1 - Customer Order Viewer

---
+++ App1 - Customer Order Viewer/CustomerOrderViewer/CustomerOrderViewer/Program.cs
@@ -0,0 +1,51 @@
+﻿using CustomerOrderViewer.Models;
+using CustomerOrderViewer.Repository;
+using System;
+using System.Data.SqlClient;
+
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Hello World!");
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=localhost;
+                                                                                                     Initial Catalog=CustomerOrderViewer;
+                                                                                                     Integrated Security=True;
+                                                                                                     Encrypt=False;
+                                                                                                     Trust Server Certificate=True");
+
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())
+                {
+                    foreach (CustomerOrderDetailModel customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine("{0}: Fullname: {1} {2} (Id: {3}) - purchased {4} for {5} (Id: {6})",
+                            customerOrderDetailModel.CustomerOrderId,
+                            customerOrderDetailModel.FirstName,
+                            customerOrderDetailModel.LastName,
+                            customerOrderDetailModel.CustomerId,
+                            customerOrderDetailModel.Description,
+                            customerOrderDetailModel.Price,
+                            customerOrderDetailModel.ItemId
+                            );
+
+                    }
+
+
+                }
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong {0}", ex.Message);
+            }
+
+        }
+    }
+}
